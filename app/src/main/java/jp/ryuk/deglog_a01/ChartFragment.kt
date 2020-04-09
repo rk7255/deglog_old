0602e448fdcc5d7b91f392643e35477fe28d67c5
ryuk <acaucans@gmail.com>
2020-04-09 16:26:24 +0900

    BottomNavigationBar実装

---
+++ app/src/main/java/jp/ryuk/deglog_a01/ChartFragment.kt
@@ -0,0 +1,22 @@
+package jp.ryuk.deglog_a01
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ChartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chart, container, false)
+    }
+
+}
--- app/src/main/java/jp/ryuk/deglog_a01/MainActivity.kt
+++ app/src/main/java/jp/ryuk/deglog_a01/MainActivity.kt
@@ -2,11 +2,19 @@ package jp.ryuk.deglog_a01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+        NavigationUI.setupWithNavController(bottom_navigation_bar, navController)
     }
+
+
 }
